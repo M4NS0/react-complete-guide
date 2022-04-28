a2bf503130669fed671aa2c3613d98ea1fdfe266
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-04-28 11:22:33 -0300

    Creating new component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   // JSX  code:
   return (   
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
