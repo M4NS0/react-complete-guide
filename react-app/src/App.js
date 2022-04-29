0758e45a96d57f2ec167055fd25bea8e2dd69e8a
Bruno C. Manso <mrbrunomanso@gmail.com>
2022-04-29 09:18:49 -0300

    Adding form inputs

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import Expenses from './components/Expenses/Expenses';
-// import React from 'react'
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -23,17 +24,11 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
 ];
-  // Alternative:
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get Started"),
-  //   React.createElement(Expenses, { items: expenses }),
-  // );
+
 
   return (   
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
